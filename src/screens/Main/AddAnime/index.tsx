128b467f5898c72031f27c415e1117462b39769e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-01-06 17:00:03 -0300

    feat: add design of save file

--- src/screens/Main/AddAnime/index.tsx
+++ src/screens/Main/AddAnime/index.tsx
@@ -1,16 +1,59 @@
-import {ScrollView, TouchableOpacity} from 'react-native';
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
 import styles from './styles';
-import React from 'react';
-const AddAnime = () => (
-  <ScrollView style={styles.container}>
-    <TouchableOpacity
-      style={styles.buttonGetImage}
-      onPress={async () => {
-        const result = await launchImageLibrary();
-      }}
-    />
-  </ScrollView>
-);
+import React, {useState} from 'react';
+const AddAnime = () => {
+  const [image, setImage] = useState('');
+
+  return (
+    <ScrollView style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttonGetImage}
+        onPress={async () => {
+          try {
+            const result = await launchImageLibrary({
+              mediaType: 'photo',
+            });
+            if (!result.assets) {
+              return;
+            }
+            setImage(result?.assets[0]?.uri);
+          } catch {
+            Alert.alert('JSON.stringify(result)');
+
+            console.log('error');
+          }
+        }}>
+        <Image
+          source={{uri: image}}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+      <TextInput style={styles.input} placeholder="Nome do anime" />
+      <View style={styles.row}>
+        <TextInput placeholder="Nota" style={styles.input} />
+
+        <TextInput
+          style={[styles.input, {marginHorizontal: 16}]}
+          placeholder="N Episódios"
+        />
+        <TextInput style={styles.input} placeholder="Data" />
+      </View>
+      <TextInput textAlignVertical="top" multiline style={styles.textarea} />
+      <TouchableOpacity style={styles.buttonSave}>
+        <Text style={styles.buttonSaveText}>Salvar</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
 
 export default AddAnime;
--- src/screens/Main/AddAnime/styles.ts
+++ src/screens/Main/AddAnime/styles.ts
@@ -5,6 +5,7 @@ export default StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     paddingTop: 16,
+    paddingBottom: 32,
     backgroundColor: '#fff',
   },
   buttonGetImage: {
@@ -12,5 +13,41 @@ export default StyleSheet.create({
     backgroundColor: '#D9D9D9',
     borderWidth: 1,
     borderStyle: 'dashed',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  input: {
+    backgroundColor: '#D9D9D9',
+    borderRadius: 5,
+    marginTop: 16,
+    paddingLeft: 8,
+    flex: 1,
+    fontSize: 16,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  textarea: {
+    minHeight: 190,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginTop: 16,
+    borderRadius: 5,
+  },
+
+  buttonSave: {
+    backgroundColor: '#CC0000',
+    borderRadius: 5,
+    height: 47,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 32,
+    marginBottom: 32,
+  },
+  buttonSaveText: {
+    color: '#fff',
+    fontSize: 24,
   },
 });
