ef30129e8edfb57fe256d3320978e2de6c6bd974
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-01-13 16:30:33 -0300

    feat: add formik in AddAnime screen

--- src/screens/Main/AddAnime/index.tsx
+++ src/screens/Main/AddAnime/index.tsx
@@ -9,9 +9,27 @@ import {
 } from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
 import styles from './styles';
-import React, {useState} from 'react';
+import React from 'react';
+import {useFormik} from 'formik';
+import newAnime from '../../../services/api/newAnime';
+
 const AddAnime = () => {
-  const [image, setImage] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      url: '',
+      name: '',
+      episodeNumbers: '',
+      category: '',
+      review: '',
+      date: '',
+      resume: '',
+    },
+    async onSubmit(values) {
+      try {
+        await newAnime(values);
+      } catch {}
+    },
+  });
 
   return (
     <ScrollView style={styles.container}>
@@ -22,26 +40,31 @@ const AddAnime = () => {
             const result = await launchImageLibrary({
               mediaType: 'photo',
             });
+
             if (!result.assets) {
               return;
             }
-            setImage(result?.assets[0]?.uri);
+            formik.setFieldValue('url', result?.assets[0]?.uri);
           } catch {
             Alert.alert('JSON.stringify(result)');
-
-            console.log('error');
           }
         }}>
         <Image
-          source={{uri: image}}
+          source={{uri: formik.values.url}}
           style={styles.image}
           resizeMode="contain"
         />
       </TouchableOpacity>
-      <TextInput style={styles.input} placeholder="Nome do anime" />
+      <TextInput
+        style={styles.input}
+        value={formik.values.name}
+        placeholder="Nome do anime"
+      />
       <View style={styles.row}>
         <TextInput
           placeholder="Nota"
+          keyboardType="decimal-pad"
+          value={formik.values.review}
           style={[styles.input, {marginRight: 16}]}
         />
 
@@ -49,12 +72,22 @@ const AddAnime = () => {
       </View>
       <View style={styles.row}>
         <TextInput
+          value={formik.values.category}
           placeholder="Categoria"
           style={[styles.input, {marginRight: 16}]}
         />
-        <TextInput style={styles.input} placeholder="Data" />
+        <TextInput
+          value={formik.values.date}
+          style={styles.input}
+          placeholder="Data"
+        />
       </View>
-      <TextInput textAlignVertical="top" multiline style={styles.textarea} />
+      <TextInput
+        value={formik.values.resume}
+        textAlignVertical="top"
+        multiline
+        style={styles.textarea}
+      />
       <TouchableOpacity style={styles.buttonSave}>
         <Text style={styles.buttonSaveText}>Salvar</Text>
       </TouchableOpacity>
